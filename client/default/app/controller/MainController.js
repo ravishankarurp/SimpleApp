f0469ae982801ecfbb716512c853f01258ee8c2a
Ravi Parupudi <ravishankarurp@gmail.com>
2013-02-21 12:43:23 +0000

    2.2

--- client/default/app/controller/MainController.js
+++ client/default/app/controller/MainController.js
@@ -15,7 +15,44 @@ Ext.define('SimpleApp.controller.MainController', {
     
     //called when the Application is launched, remove if not needed
     launch: function(app) {
-        console.log('Application Loaded');
+        console.log('Application Loaded. Inserting record');
+		
+		//create a data record for entity "myFirstEntity"
+		$fh.db({
+		  "act": "create",
+		  "type": "myFirstEntity",
+		  "fields": {
+		    "firstName": "Joe",
+		    "lastName": "Bloggs",
+		    "address1": "22 Blogger Lane",
+		    "address2": "Bloggsville",
+		    "country": "Bloggland",
+		    "phone": "555-123456"
+		  }
+		}, function(err, data) {
+		  if (err) {
+		    console.log("Error " + err)
+		  } else {
+		    console.log(JSON.stringify(data))
+		    /*
+		     The output will be something similar to this
+		     {
+		      "fields": {
+		        "address1": "22 Blogger Lane",
+		        "address2": "Bloggsville",
+		        "country": "Bloggland",
+		        "fistName": "Joe",
+		        "lastName": "Bloggs",
+		        "phone": "555-123456"
+		      },
+		      "guid": "4e563ea44fe8e7fc19000002",
+		      "type": "myFirstEntity"
+		    }
+		    */
+		  }
+		});
+		
+		console.log('Inserted record..');
     },
 	
 	tempInCelsiusSubmitted: function(){
@@ -24,7 +61,32 @@ Ext.define('SimpleApp.controller.MainController', {
 		console.log('Submitted Celsius button. Value is ' + celsiusValue);
 		
 		$fh.log({
-		  message: 'your log message'
+		  message: 'Querying inserted record... '
 		});
+		
+		$fh.db({
+		  "act": "read",
+		  "type": "myFirstEntity",
+		  "guid": celsiusValue
+		}, function(err, data) {
+			console.log('Result: ' + JSON.stringify(data));
+			
+		  /* Sample output 
+		    {
+		      "fields": {
+		        "address1": "22 Blogger Lane",
+		        "address2": "Bloggsville",
+		        "country": "Bloggland",
+		        "fistName": "Joe",
+		        "lastName": "Bloggs",
+		        "phone": "555-123456"
+		      },
+		      "guid": "4e563ea44fe8e7fc19000002",
+		      "type": "myFirstEntity"
+		    }
+		    */
+		});
+		
+		console.log('Done...');
 	}
 });
